ec08b99c106256e8ed4c13b041d0c2b7d967ebf9
mezealot24 <wiriya.on@gmail.com>
2024-06-11 22:15:42 +0700

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,43 +14,48 @@ import { ContactUs } from "@/pages/contact-us/ContactUs.jsx";
 import { ProductsList } from "@/pages/products-list/ProductsList.jsx";
 import { ProductDetail } from "@/pages/product-detail/ProductDetail.jsx";
 import DevDocument from "./pages/DevDocument";
+import SignIn from "@/pages/sign-in/Sign-in.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/horoscope",
-        element: <Horoscope />,
-      },
-      {
-        path: "/contact-us",
-        element: <ContactUs />,
-      },
-      {
-        path: "/products-list",
-        element: <ProductsList />,
-      },
-      {
-        path: "/product-detail",
-        element: <ProductDetail />,
-      },
-    ],
-  },
-  // Dev Document
-  {
-    path: "/document",
-    element: <DevDocument />,
-  }
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/horoscope",
+				element: <Horoscope />,
+			},
+			{
+				path: "/contact-us",
+				element: <ContactUs />,
+			},
+			{
+				path: "/products-list",
+				element: <ProductsList />,
+			},
+			{
+				path: "/product-detail",
+				element: <ProductDetail />,
+			},
+			{
+				path: "/signin",
+				element: <SignIn />,
+			},
+		],
+	},
+	// Dev Document
+	{
+		path: "/document",
+		element: <DevDocument />,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
