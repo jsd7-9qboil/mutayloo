12f3c2cb98d86311b692985d16a5d8cdceedd295
Ben <bennaat@gmail.com>
2024-06-11 11:30:34 +0700

    edit 3

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import { SliderBanner } from "./component/SliderBanner";
 export const Home = () => {
   return (
     <main>
-      
+      <h1>ฉันพุชได้เเล้ว</h1>
       <SliderBanner />
     </main>
   );
