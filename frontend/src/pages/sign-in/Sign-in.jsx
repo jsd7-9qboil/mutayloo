441d6d2aa621bee562890b50bd1b279527b4b464
mezealot24 <wiriya.on@gmail.com>
2024-06-12 13:56:56 +0700

    v1.0.2

--- frontend/src/pages/sign-in/Sign-in.jsx
+++ frontend/src/pages/sign-in/Sign-in.jsx
@@ -15,77 +15,75 @@ const SignIn = () => {
 					}}
 				></div>
 			</div>
-			<div className="mx-auto max-w-2xl text-center">
-				<h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-					Sign In
-				</h2>
-				<p className="mt-2 text-lg leading-8 text-gray-600">
-					Welcome back! Please sign in to your account.
-				</p>
-			</div>
-			<form
-				action="#"
-				method="POST"
-				className="mx-auto mt-16 max-w-xl sm:mt-20"
-			>
-				<div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-					<div className="sm:col-span-2">
-						<label
-							htmlFor="username"
-							className="block text-sm font-semibold leading-6 text-gray-900"
-						>
-							Username or Email
-						</label>
-						<div className="mt-2.5">
-							<input
-								type="text"
-								name="username"
-								id="username"
-								autoComplete="username"
-								className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								placeholder="Username or Email"
-							/>
-						</div>
-					</div>
-					<div className="sm:col-span-2">
-						<label
-							htmlFor="password"
-							className="block text-sm font-semibold leading-6 text-gray-900"
-						>
-							Password
-						</label>
-						<div className="mt-2.5">
-							<input
-								type="password"
-								name="password"
-								id="password"
-								autoComplete="current-password"
-								className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								placeholder="Password"
-							/>
-						</div>
-					</div>
-					<div className="sm:col-span-2 text-right">
-						<a href="#" className="text-sm text-primary hover:underline">
-							Forgot Your Password?
-						</a>
-					</div>
-				</div>
-				<div className="mt-10">
-					<button
-						type="submit"
-						className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-					>
+			<div className="w-full max-w-md mx-auto bg-white rounded-lg shadow-md p-8">
+				<div className="text-center">
+					<h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
 						Sign In
-					</button>
+					</h2>
+					<p className="mt-2 text-lg leading-8 text-gray-600">
+						Welcome back! Please sign in to your account.
+					</p>
 				</div>
-				<p className="text-sm text-gray-700 mt-4">
-					Don't have an account?{" "}
-					<a href="#" className="text-indigo-600 hover:underline">
-						Create account
-					</a>
-				</p>
-			</form>
+				<form action="#" method="POST" className="mt-8">
+					<div className="grid grid-cols-1 gap-y-6">
+						<div>
+							<label
+								htmlFor="username"
+								className="block text-sm font-semibold leading-6 text-gray-900"
+							>
+								Username or Email
+							</label>
+							<div className="mt-2.5">
+								<input
+									type="text"
+									name="username"
+									id="username"
+									autoComplete="username"
+									className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									placeholder="Username or Email"
+								/>
+							</div>
+						</div>
+						<div>
+							<label
+								htmlFor="password"
+								className="block text-sm font-semibold leading-6 text-gray-900"
+							>
+								Password
+							</label>
+							<div className="mt-2.5">
+								<input
+									type="password"
+									name="password"
+									id="password"
+									autoComplete="current-password"
+									className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									placeholder="Password"
+								/>
+							</div>
+						</div>
+						<div className="text-right">
+							<a href="#" className="text-sm text-indigo-600 hover:underline">
+								Forgot Your Password?
+							</a>
+						</div>
+					</div>
+					<div className="mt-6">
+						<button
+							type="submit"
+							className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						>
+							Sign In
+						</button>
+					</div>
+					<p className="text-sm text-gray-700 mt-4 text-center">
+						Don't have an account?{" "}
+						<a href="#" className="text-indigo-600 hover:underline">
+							Create account
+						</a>
+					</p>
+				</form>
+			</div>
 		</div>
 	);
 };
--- frontend/src/router/rounter.jsx
+++ frontend/src/router/rounter.jsx
@@ -7,39 +7,44 @@ import { ContactUs } from "@/pages/contact-us/ContactUs.jsx";
 import { ProductsList } from "@/pages/products-list/ProductsList.jsx";
 import { ProductDetail } from "@/pages/product-detail/ProductDetail.jsx";
 import DevDocument from "@/pages/DevDocument";
+import SignIn from "../pages/sign-in/Sign-in";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/horoscope",
-        element: <Horoscope />,
-      },
-      {
-        path: "/contact-us",
-        element: <ContactUs />,
-      },
-      {
-        path: "/products-list",
-        element: <ProductsList />,
-      },
-      {
-        path: "/product-detail",
-        element: <ProductDetail />,
-      },
-    ],
-  },
-  // Dev Document
-  {
-    path: "/document",
-    element: <DevDocument />,
-  }
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/horoscope",
+				element: <Horoscope />,
+			},
+			{
+				path: "/contact-us",
+				element: <ContactUs />,
+			},
+			{
+				path: "/products-list",
+				element: <ProductsList />,
+			},
+			{
+				path: "/product-detail",
+				element: <ProductDetail />,
+			},
+			{
+				path: "/sign-in",
+				element: <SignIn />,
+			},
+		],
+	},
+	// Dev Document
+	{
+		path: "/document",
+		element: <DevDocument />,
+	},
 ]);
 
 export default router;
