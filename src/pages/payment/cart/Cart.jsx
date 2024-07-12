3a8d42ff46f4369f18167d0d642c5c6ef3dff27e
oilly18 <oilly18@hotmail.com>
2024-07-12 17:03:23 +0700

    finish-checkout and order-complete

--- src/pages/payment/cart/Cart.jsx
+++ src/pages/payment/cart/Cart.jsx
@@ -20,8 +20,158 @@ const Cart = () => {
   return (
     <div className="container mx-auto">
       <Status />
+      <div>
+        <div>
+          <div className="md:w-full">
+            <div className="flex flex-col px-6 py-4 bg-white rounded-md border border-solid border-zinc-500 max-w-[509px] max-md:px-5">
+              <div className="text-3xl font-medium tracking-tight leading-8 text-neutral-900 max-md:max-w-full">
+                Order summary
+              </div>
+              <div className="flex flex-col justify-center py-2  border-b border-gray-200 border-solid max-md:max-w-full">
+                <div className="flex gap-4 max-md:flex-wrap max-md:max-w-full">
+                  <div className="flex flex-col justify-center">
+                    <div className="shrink-0 h-24 bg-purple-600" />
+                  </div>
+                  <div className="flex pb-1.5 gap-8">
+                    <div>
+                      <div className="flex flex-col justify-center items-start py-1.5 mt-2 rounded  max-md:pr-5">
+                        <img
+                          src="https://via.placeholder.com/80x96"
+                          className="w-4/5 h-full aspect-[3.23]"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-sm font-semibold leading-5 text-neutral-900">
+                        Tray Table
+                      </div>
+                      <div className="mt-2 text-xs leading-5 text-zinc-500">
+                        Color: Black
+                      </div>
+                      <div className="flex items-center self-stretch justify-center gap-3 px-2 py-2 rounded-sm bg-neutral-100">
+                        <button className="px-2 py-2 hover:bg-gray-300 focus:outline-none focus:ring-2">
+                          -
+                        </button>
+                        <div>2</div>
+                        <button className="px-2 py-2 rounded-sm hover:bg-gray-300 focus:outline-none focus:ring-2">
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <div className="justify-end items-end pl-20 ">390 บาท</div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center py-2  border-b border-gray-200 border-solid max-md:max-w-full">
+                <div className="flex gap-4 max-md:flex-wrap max-md:max-w-full">
+                  <div className="flex flex-col justify-center">
+                    <div className="shrink-0 h-24 bg-purple-600" />
+                  </div>
+                  <div className="flex pb-1.5 gap-8">
+                    <div>
+                      <div className="flex flex-col justify-center items-start py-1.5 mt-2 rounded  max-md:pr-5">
+                        <img
+                          src="https://via.placeholder.com/80x96"
+                          className="w-4/5 h-full aspect-[3.23]"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-sm font-semibold leading-5 text-neutral-900">
+                        Tray Table
+                      </div>
+                      <div className="mt-2 text-xs leading-5 text-zinc-500">
+                        Color: Black
+                      </div>
+                      <div className="flex items-center self-stretch justify-center gap-3 px-2 py-2 rounded-sm bg-neutral-100">
+                        <button className="px-2 py-2 hover:bg-gray-300 focus:outline-none focus:ring-2">
+                          -
+                        </button>
+                        <div>2</div>
+                        <button className="px-2 py-2 rounded-sm hover:bg-gray-300 focus:outline-none focus:ring-2">
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <div className="justify-end items-end pl-20 ">390 บาท</div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center py-2  border-b border-gray-200 border-solid max-md:max-w-full">
+                <div className="flex gap-4 max-md:flex-wrap max-md:max-w-full">
+                  <div className="flex flex-col justify-center">
+                    <div className="shrink-0 h-24 bg-purple-600" />
+                  </div>
+                  <div className="flex pb-1.5 gap-8">
+                    <div>
+                      <div className="flex flex-col justify-center items-start py-1.5 mt-2 rounded  max-md:pr-5">
+                        <img
+                          src="https://via.placeholder.com/80x200"
+                          className="w-4/5 h-full aspect-[3.23]"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-sm font-semibold leading-5 text-neutral-900">
+                        Tray Table
+                      </div>
+                      <div className="mt-2 text-xs leading-5 text-zinc-500">
+                        Color: Black
+                      </div>
+                      <div className="flex items-center self-stretch justify-center gap-3 px-2 py-2 rounded-sm bg-neutral-100">
+                        <button className="px-2 py-2 hover:bg-gray-300 focus:outline-none focus:ring-2">
+                          -
+                        </button>
+                        <div>2</div>
+                        <button className="px-2 py-2 rounded-sm hover:bg-gray-300 focus:outline-none focus:ring-2">
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <div className="justify-end items-end pl-20 ">390 บาท</div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-3 mt-6 text-base ">
+                <input
+                  type="text"
+                  className="w-2/3 justify-center px-4 py-3.5 bg-white rounded-md border border-solid border-stone-300 leading-[162.5%] text-zinc-600"
+                  placeholder="Input"
+                />
+                <button className="w-1/3 justify-center  px-7 py-3 font-medium tracking-tight leading-7 text-center text-white bg-purple-600 rounded-lg max-md:px-5">
+                  Apply
+                </button>
+              </div>
+              <div className="flex flex-col justify-center py-3.5 mt-4 text-base leading-7 border-b border-gray-200 border-solid max-md:max-w-full">
+                <div className="flex gap-5 justify-center w-full max-md:flex-wrap max-md:max-w-full">
+                  <div className="flex gap-2 whitespace-nowrap text-neutral-900">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/cebfcb5e4788737dcb6dc75251bba3ca921ac5d4f139f9f21b869fccfd6aa970?"
+                      className="shrink-0 self-start w-6 aspect-square"
+                    />
+                    <div>Mutayloo</div>
+                  </div>
+                  <div className="flex-auto my-auto font-semibold text-right text-green-700">
+                    -$20.00 [Remove]
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center py-3.5 text-base leading-7 whitespace-nowrap border-b border-gray-200 border-solid text-neutral-900 max-md:max-w-full">
+                <div className="flex gap-5 justify-center max-md:flex-wrap max-md:max-w-full">
+                  <div>Shipping</div>
+                  <div className="my-auto font-semibold text-right">Free</div>
+                </div>
+              </div>
+              <div className="flex gap-5 justify-center mt-3.5 text-xl font-medium leading-7 whitespace-nowrap text-neutral-900 max-md:flex-wrap max-md:max-w-full">
+                <div>Total</div>
+                <div className="my-auto text-right">$95.00</div>
+              </div>
+            </div>
+          </div>
 
-      <div className="flex space-x-4">
+        </div>
+        <div className="flex space-x-4">
         <div className="w-3/4">
           {cartItems.map((item) => (
             <CartItem key={item.id} item={item} />
@@ -37,6 +187,15 @@ const Cart = () => {
           />
         </div>
       </div>
+
+
+
+
+
+      </div>
+      
+
+      
     </div>
   );
 };
--- src/pages/payment/cart/OrderComplete.jsx
+++ src/pages/payment/cart/OrderComplete.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const OrderComplete = () => {
   return (
-    <div>
-            <div className="min-w-min">
-        <div>
+    <div >
+      <div className="min-w-min">
+        <div className="m-4">
           <div className="lg:max-w-4xl w-full  mx-auto ">
-            <h1 className="text-center font-bold my-8">Complete !!</h1>
+            <h1 className="text-center font-bold mt-12 mb-8">Complete !!</h1>
             <div className="grid grid-cols-3 gap-8">
-              <div className="hidden lg:flex flex items-center gap-4 lg:block">
+              <div className="hidden lg:flex items-center gap-4 lg:block">
                 <div
                   className="lg:w-12 w-8 lg:h-12 h-5 rounded-full  
                     inline-flex items-center justify-center  
@@ -18,21 +18,21 @@ const OrderComplete = () => {
                 </div>
                 <p className="lg:text-xl font-bold">Shopping Cart</p>
               </div>
-              <div className="flex items-center gap-4 ">
+              <div className="hidden lg:block flex lg:flex items-center gap-4 ">
                 <div
-                  className="lg:w-12 w-8 lg:h-12 h-5  rounded-full  
-                    inline-flex items-center justify-center  
-                    bg-primary text-white text-xl font-bold text-center"
+                  className="lg:w-12 w-8 lg:h-12 h-5 rounded-full  
+                  inline-flex items-center justify-center  
+                  bg-primary text-white text-xl font-bold"
                 >
                   2
                 </div>
-                <p className="lg:text-xl font-bold text-center">Checkout Detail</p>
+                <p className="lg:text-xl font-bold ">Checkout Detail</p>
               </div>
-              <div className="hidden lg:block lg:flex flex items-center gap-4">
+              <div className="lg:block lg:flex flex items-center gap-4">
                 <div
-                  className="lg:w-12 w-8 lg:h-12 h-5  rounded-full  
-                    inline-flex items-center justify-center  
-                    bg-primary text-white text-xl font-bold"
+                  className="lg:w-12 w-8 lg:h-12 h-5 rounded-full  
+                  inline-flex items-center justify-center  
+                  bg-primary text-white text-xl font-bold"
                 >
                   3
                 </div>
@@ -42,15 +42,72 @@ const OrderComplete = () => {
           </div>
         </div>
       </div>
-
-
-
-
-
-
-
+      <div>
+       <div className="flex flex-col items-center py-2 bg-white">
+        <div className="flex justify-center items-center self-stretch px-16 py-8 lg:py-20  w-full bg-white rounded-lg shadow-2xl max-md:px-5 max-md:mt-10 max-md:max-w-full">
+            <div className="flex flex-col max-w-full w-[546px]">
+              <div className="flex flex-col px-7 font-medium text-center max-md:px-5 max-md:max-w-full">
+                <div className="self-center text-3xl tracking-tight leading-8 text-zinc-500 max-md:max-w-full">
+                ขอบคุณ 🎉
+                </div>
+                <div className="mt-4 text-3xl tracking-tight leading-10 text-zinc-800 max-md:max-w-full">
+                เราได้รับรายการสั่งซื้อ
+                <br />
+                ของคุณเรียบร้อย
+                </div>
+            </div>
+            <div className="justify-center px-20 mt-10 max-md:px-5 max-md:max-w-full">
+                <div className="flex gap-5">
+                  <div className="flex  w-[33%] max-md:ml-0 max-md:w-full">
+                    <div className="flex flex-col grow pb-5 text-base font-semibold leading-6 text-center text-gray-50 whitespace-nowrap max-md:mt-10">
+                      <div className="z-10 justify-center items-start self-end px-3 py-2.5 w-10 bg-neutral-900 rounded-[80px]">
+                        2
+                      </div>
+                    <div className="shrink-0 -mt-4 bg-purple-600 h-[116px]" />
+                  </div>
+                </div>
+                <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+                    <div className="flex flex-col grow pb-5 text-base font-semibold leading-6 text-center text-gray-50 whitespace-nowrap max-md:mt-10">
+                      <div className="z-10 justify-center items-start self-end px-3 py-2.5 w-10 bg-neutral-900 rounded-[80px]">
+                        2
+                      </div>
+                      <div className="shrink-0 -mt-4 bg-purple-600 h-[116px]" />
+                    </div>
+                </div>
+                <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+                    <div className="flex flex-col grow pb-5 text-base font-semibold leading-6 text-center text-gray-50 whitespace-nowrap max-md:mt-10">
+                      <div className="z-10 justify-center items-start self-end px-3.5 py-2.5 w-10 bg-neutral-900 rounded-[80px]">
+                        1
+                      </div>
+                      <div className="shrink-0 -mt-4 bg-purple-600 h-[116px]" />
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div className="justify-center mx-7 mt-10 max-md:mr-2.5 max-md:max-w-full">
+                <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+                  <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+                    <div className="flex flex-col grow text-sm font-semibold leading-5 text-zinc-500 max-md:mt-8">
+                    <div>Order number:</div>
+                    <div className="mt-5">Date:</div>
+                    <div className="mt-5">Email:</div>
+                    <div className="mt-5">Total:</div>
+                    <div className="mt-5">Payment method:</div>
+                   </div>
+                  </div>
+                </div>
+            </div>
+            <div className="justify-center self-center px-10 py-3 mt-10 text-base font-medium tracking-tight leading-7 text-center text-white bg-purple-600 max-md:px-5">
+                <button>
+                Purchase history
+                </button>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
     </div>
-  )
-}
+  );
+};
 
 export default OrderComplete;
