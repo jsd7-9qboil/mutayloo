200656c39ea6e5d0616fd4ee689b9589bb74ca0a
oilly18 <oilly18@hotmail.com>
2024-07-17 02:25:22 +0700

    update horoscope

--- src/pages/horoscope/Horoscope.jsx
+++ src/pages/horoscope/Horoscope.jsx
@@ -10,13 +10,13 @@ const Horoscope = () => {
     <div>
       <BreadcrumbBanner />
       <section className="container">
-        <h2>Horoscope</h2>
-        <div className="grid grid-cols-4 gap-4">
+        <h2 className="mt-8 font-bold">Horoscope</h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-4 lg:py-24 my-8 lg:mr-8 justify-center items-center lg:ml-16 ">
           {horoscopeData.map((horoscope) => (
             <Link to={`/horoscope/${horoscope.id}`} key={horoscope.id}>
-              <div key={horoscope.id}>
-                <p>{horoscope.name}</p>
-                <p>{horoscope.description}</p>
+              <div className="bg-gray-200 justify-center items-center pl-10 md:pl-14 lg:ml-24 py-8 rounded-full lg:rounded-full lg:pl-20" key={horoscope.id}>
+                <p className="justify-center items-center pl-4  text-2xl">{horoscope.name}</p>
+                <p className="pl-2 ">{horoscope.description}</p>
                 <img src={horoscope.image} alt="" />
               </div>
             </Link>
--- src/pages/horoscope/components/HoroscopeSymbol.jsx
+++ src/pages/horoscope/components/HoroscopeSymbol.jsx
@@ -4,7 +4,7 @@ export const HoroscopeSymbol = ({  }) => {
   console.log(horoscope);
   return (
     <div className="container">
-      <div className="grid grid-cols-2 lg:grid lg:grid-cols-4 md:grid md:grid-cols-3 w-full h-full">
+      <div className="grid grid-cols-2 lg:grid lg:grid-cols-4 md:grid md:grid-cols-3 w-full h-full ">
         <img src={horoscope.img} className="w-full aspect-square" />
       </div>
     </div>
